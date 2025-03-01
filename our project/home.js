0bcd8c0902e7d51a4adf7756a569d8effa4514d0
aketo1 <148247250+aketo1@users.noreply.github.com>
2025-03-01 23:22:37 +0300

    Delete our project/home.js

--- our project/home.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('menuBtn').addEventListener('click', function() {
-    document.getElementById('sidebar').classList.add('active');
-    document.getElementById('sidebar').style.left = '0';
-});
-
-document.getElementById('closeBtn').addEventListener('click', function() {
-    document.getElementById('sidebar').classList.remove('active');
-    document.getElementById('sidebar').style.left = '-250px';
-});
